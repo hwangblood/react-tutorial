df9253b244a49234ce6ab5571176d17bd92262b3
hwangblood <hwangblood@gmail.com>
2023-01-03 00:00:08 +0800

    Chapter 17-18: Router Hooks & Links, Flexbox Components

--- README.md
+++ README.md
@@ -36,9 +36,11 @@ This React JS Full Course for Beginners is an all-in-one beginner tutorial and c
 
 ([4:04:33](https://www.youtube.com/watch?v=RVFAyFWO4go&t=14673s)) Chapter 15: Fetch Data Challenge
 
-### blog_app (Chapter 16)
+### blog_app (Chapter 16-18)
 
 ([4:43:10](https://www.youtube.com/watch?v=RVFAyFWO4go&t=16990s)) Chapter 16: React Router (v5)
+([5:02:58](https://www.youtube.com/watch?v=RVFAyFWO4go&t=18178s)) Chapter 17: Router Hooks & Links
+([5:51:28](https://www.youtube.com/watch?v=RVFAyFWO4go&t=21088s)) Chapter 18: Flexbox Components
 
 ......
 
--- blog_app/src/NewPost.js
+++ blog_app/src/NewPost.js
@@ -1,9 +1,37 @@
 import React from "react";
 
-const NewPost = () => {
+const NewPost = ({
+  handleSubmit,
+  postTitle,
+  setPostTitle,
+  postBody,
+  setPostBody,
+}) => {
   return (
-    <main>
-      <h1>NewPost</h1>
+    <main className="NewPost">
+      <h2>NewPost</h2>
+
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          required
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
+        />
+        <label htmlFor="body">Title:</label>
+        <textarea
+          type="text"
+          name="body"
+          id="body"
+          required
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </main>
   );
 };
